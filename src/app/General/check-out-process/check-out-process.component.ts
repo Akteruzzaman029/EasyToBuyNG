5575a2ec668d1ef275e779cfb0172220d1ec4e3e
Akteruzzaman-codezzi <akteruzzaman.codezzi@gmail.com>
2026-01-27 00:59:49 +0600

    updated

--- src/app/General/check-out-process/check-out-process.component.ts
+++ src/app/General/check-out-process/check-out-process.component.ts
@@ -14,23 +14,29 @@ import { HttpHelperService } from '../../Shared/Service/http-helper.service';
 import { OrderRequestDto } from '../../Model/Order';
 import { OrderItemRequestDto } from '../../Model/OrderItem';
 import { AddressFilterDto, AddressRequestDto } from '../../Model/Address';
-import { DeliveryAddressComponent } from "../delivery-address/delivery-address.component";
+import { DeliveryAddressComponent } from '../delivery-address/delivery-address.component';
 import { LoginRequestDto } from '../../Model/LoginRequestDto';
 
 @Component({
   selector: 'app-check-out-process',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterModule, TruncatePipe, DeliveryAddressComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    TruncatePipe,
+    DeliveryAddressComponent,
+  ],
   templateUrl: './check-out-process.component.html',
   styleUrl: './check-out-process.component.scss',
-  providers: [DatePipe, { provide: LOCALE_ID, useValue: 'en-US' }]
+  providers: [DatePipe, { provide: LOCALE_ID, useValue: 'en-US' }],
 })
 export class CheckOutProcessComponent implements OnInit, OnDestroy {
   public oCartFilterDto = new CartFilterDto();
   public oCurrentUser = new UserResponseDto();
-  public oCartRequestDto = new CartRequestDto()
+  public oCartRequestDto = new CartRequestDto();
   public oAddressRequestDto: any = {};
-  public oAddressFilterDto = new AddressFilterDto()
+  public oAddressFilterDto = new AddressFilterDto();
   public cartItemList: any[] = [];
   private subscription: Subscription | any;
 
@@ -46,14 +52,14 @@ export class CheckOutProcessComponent implements OnInit, OnDestroy {
     private http: HttpHelperService,
     private cartService: CartService,
     private route: Router,
-    private datePipe: DatePipe
+    private datePipe: DatePipe,
   ) {
     this.oCurrentUser = CommonHelper.GetUser();
   }
 
   ngOnInit(): void {
     if (this.oCurrentUser.userId == '') {
-      this.route.navigateByUrl("login");
+      this.route.navigateByUrl('login');
     }
     this.subscription = this.cartService.onCartUpdated().subscribe(() => {
       this.loadCart();
@@ -78,8 +84,12 @@ export class CheckOutProcessComponent implements OnInit, OnDestroy {
         this.cartItemList = res;
       },
       (err) => {
-        this.toast.error(err?.ErrorMessage || 'Failed to load cart.', 'Error!!', { progressBar: true });
-      }
+        this.toast.error(
+          err?.ErrorMessage || 'Failed to load cart.',
+          'Error!!',
+          { progressBar: true },
+        );
+      },
     );
   }
 
@@ -90,13 +100,13 @@ export class CheckOutProcessComponent implements OnInit, OnDestroy {
     this.oCartRequestDto.productId = item.productId;
     this.oCartRequestDto.quantity = 1;
     this.oCartRequestDto.actionType = 1;
-    this.http.Post("Cart/InsertCart", this.oCartRequestDto).subscribe(
+    this.http.Post('Cart/InsertCart', this.oCartRequestDto).subscribe(
       (res) => {
         this.cartService.notifyCartUpdated(); // Notify cart update
       },
       (err) => {
-        this.toast.error(err.ErrorMessage, "Error!!", { progressBar: true });
-      }
+        this.toast.error(err.ErrorMessage, 'Error!!', { progressBar: true });
+      },
     );
   }
 
@@ -107,13 +117,13 @@ export class CheckOutProcessComponent implements OnInit, OnDestroy {
     this.oCartRequestDto.productId = item.productId;
     this.oCartRequestDto.quantity = 1;
     this.oCartRequestDto.actionType = -1;
-    this.http.Post("Cart/InsertCart", this.oCartRequestDto).subscribe(
+    this.http.Post('Cart/InsertCart', this.oCartRequestDto).subscribe(
       (res) => {
         this.cartService.notifyCartUpdated(); // Notify cart update
       },
       (err) => {
-        this.toast.error(err.ErrorMessage, "Error!!", { progressBar: true });
-      }
+        this.toast.error(err.ErrorMessage, 'Error!!', { progressBar: true });
+      },
     );
   }
 
@@ -124,13 +134,13 @@ export class CheckOutProcessComponent implements OnInit, OnDestroy {
     this.oCartRequestDto.productId = item.productId;
     this.oCartRequestDto.quantity = 1;
     this.oCartRequestDto.actionType = 0;
-    this.http.Post("Cart/InsertCart", this.oCartRequestDto).subscribe(
+    this.http.Post('Cart/InsertCart', this.oCartRequestDto).subscribe(
       (res) => {
         this.cartService.notifyCartUpdated(); // Notify cart update
       },
       (err) => {
-        this.toast.error(err.ErrorMessage, "Error!!", { progressBar: true });
-      }
+        this.toast.error(err.ErrorMessage, 'Error!!', { progressBar: true });
+      },
     );
   }
 
@@ -139,11 +149,17 @@ export class CheckOutProcessComponent implements OnInit, OnDestroy {
   }
 
   getSubTotal(): number {
-    return this.cartItemList.reduce((sum, item) => sum + item.purchasePrice * item.quantity, 0);
+    return this.cartItemList.reduce(
+      (sum, item) => sum + item.purchasePrice * item.quantity,
+      0,
+    );
   }
 
   getTotalDiscount(): number {
-    return this.cartItemList.reduce((sum, item) => sum + item.discount * item.quantity, 0);
+    return this.cartItemList.reduce(
+      (sum, item) => sum + item.discount * item.quantity,
+      0,
+    );
   }
 
   getGrandTotal(): number {
@@ -151,26 +167,28 @@ export class CheckOutProcessComponent implements OnInit, OnDestroy {
   }
 
   checkout(): void {
-
-
     if (this.selectedPayment == 'online') {
       this.InsertOrder();
-    }
-    else if (this.selectedPayment == 'cash') {
+    } else if (this.selectedPayment == 'cash') {
       this.InsertOrder();
     } else {
-      this.toast.warning("Please select payment method", "Warning!!", { progressBar: true });
+      this.toast.warning('Please select payment method', 'Warning!!', {
+        progressBar: true,
+      });
       return;
     }
-
   }
 
-
   InsertOrder(): void {
-
-    this.oOrderRequestDto.orderType = Number(this.selectedPayment == 'cash' ? 1 : this.selectedPayment == 'online' ? 2 : 0);
+    this.oOrderRequestDto.orderType = Number(
+      this.selectedPayment == 'cash'
+        ? 1
+        : this.selectedPayment == 'online'
+          ? 2
+          : 0,
+    );
     this.oOrderRequestDto.orderStatus = 1;
-    this.oOrderRequestDto.totalAmount =Number(this.getGrandTotal());
+    this.oOrderRequestDto.totalAmount = Number(this.getGrandTotal());
     this.oOrderRequestDto.totalDiscount = Number(this.getTotalDiscount());
     this.oOrderRequestDto.addressId = Number(this.oAddressRequestDto.id);
     this.oOrderRequestDto.companyId = Number(CommonHelper.GetComapyId());
@@ -188,80 +206,85 @@ export class CheckOutProcessComponent implements OnInit, OnDestroy {
       this.oOrderRequestDto.orderItems.push(oOrderItemRequestDto);
     });
 
-    if(this.oOrderRequestDto.userId==""){
-       this.toast.warning("Please login", "Warning!!", { progressBar: true });
-       return;
+    if (this.oOrderRequestDto.userId == '') {
+      this.toast.warning('Please login', 'Warning!!', { progressBar: true });
+      return;
     }
 
-    if(this.oOrderRequestDto.addressId==0){
-       this.toast.warning("Please select address", "Warning!!", { progressBar: true });
-       return;
+    if (this.oOrderRequestDto.addressId == 0) {
+      this.toast.warning('Please select address', 'Warning!!', {
+        progressBar: true,
+      });
+      return;
     }
-    if(this.oOrderRequestDto.totalAmount<0){
-       this.toast.warning("Amount must be geather 0", "Warning!!", { progressBar: true });
-       return;
+    if (this.oOrderRequestDto.totalAmount < 0) {
+      this.toast.warning('Amount must be geather 0', 'Warning!!', {
+        progressBar: true,
+      });
+      return;
     }
 
-    this.http.Post("Order/InsertOrder", this.oOrderRequestDto).subscribe(
+    this.http.Post('Order/InsertOrder', this.oOrderRequestDto).subscribe(
       (res) => {
         this.cartService.notifyCartUpdated(); // Notify cart update
 
         if (this.selectedPayment == 'online') {
-          this.toast.success("Order Online Payment", "Success!!", { progressBar: true });
-
-        }
-        else if (this.selectedPayment == 'cash') {
-          this.toast.success("Order Cash on Delivery", "Success!!", { progressBar: true });
+          this.toast.success('Order Online Payment', 'Success!!', {
+            progressBar: true,
+          });
+        } else if (this.selectedPayment == 'cash') {
+          this.toast.success('Order Cash on Delivery', 'Success!!', {
+            progressBar: true,
+          });
         } else {
-
-
         }
+        this.route.navigateByUrl('orders');
       },
       (err) => {
-        this.toast.error(err.ErrorMessage, "Error!!", { progressBar: true });
-      }
+        this.toast.error(err.ErrorMessage, 'Error!!', { progressBar: true });
+      },
     );
   }
 
-
   public Login() {
-
-    if (this.oLoginRequestDto.UserName == "") {
-      this.toast.warning("Enter your user name!!", "Warning!!", { progressBar: true });
+    if (this.oLoginRequestDto.UserName == '') {
+      this.toast.warning('Enter your user name!!', 'Warning!!', {
+        progressBar: true,
+      });
       return;
     }
 
-    if (this.oLoginRequestDto.Password == "") {
-      this.toast.warning("Enter your password!!", "Warning!!", { progressBar: true });
+    if (this.oLoginRequestDto.Password == '') {
+      this.toast.warning('Enter your password!!', 'Warning!!', {
+        progressBar: true,
+      });
       return;
     }
 
     const payload = {
       userName: this.oLoginRequestDto.UserName,
       password: this.oLoginRequestDto.Password,
-    }
-    this.http.Login("Auth/Login", payload).subscribe(
+    };
+    this.http.Login('Auth/Login', payload).subscribe(
       (res: any) => {
         console.log(res);
         if (res.data != null) {
-          this.toast.success("Login Successfully!!", "Success!!", { progressBar: true });
-          localStorage.setItem("Token", res.data.jwtToken)
-          localStorage.setItem("UserResponseDto", JSON.stringify(res.data))
-          CommonHelper.CommonButtonClick("closeCommonLogin");
+          this.toast.success('Login Successfully!!', 'Success!!', {
+            progressBar: true,
+          });
+          localStorage.setItem('Token', res.data.jwtToken);
+          localStorage.setItem('UserResponseDto', JSON.stringify(res.data));
+          CommonHelper.CommonButtonClick('closeCommonLogin');
         } else {
-          this.toast.error(res?.message, "Error!!", { progressBar: true });
+          this.toast.error(res?.message, 'Error!!', { progressBar: true });
         }
       },
       (err) => {
-
-        console.log(err)
-        this.toast.error(err.error.message, "Error!!", { progressBar: true });
-      });
-
+        console.log(err);
+        this.toast.error(err.error.message, 'Error!!', { progressBar: true });
+      },
+    );
   }
 
-  Registration() {
-
-  }
-
+  Registration() {}
 }
