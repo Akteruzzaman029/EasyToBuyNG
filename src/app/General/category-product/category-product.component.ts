625a18123f3a8596639291c6d35b99134041f01c
Akteruzzaman-codezzi <akteruzzaman.codezzi@gmail.com>
2026-02-21 06:56:16 +0600

    updated product category

--- src/app/General/category-product/category-product.component.ts
+++ src/app/General/category-product/category-product.component.ts
@@ -1,28 +1,26 @@
 import { CommonModule, DatePipe } from '@angular/common';
 import { Component, LOCALE_ID, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
-import { ProductFilterDto } from '../../Model/Product';
 import { ToastrService } from 'ngx-toastr';
+import { CartRequestDto } from '../../Model/Cart';
+import { ProductFilterDto } from '../../Model/Product';
 import { UserResponseDto } from '../../Model/UserResponseDto';
 import { AuthService } from '../../Shared/Service/auth.service';
+import { CartService } from '../../Shared/Service/cart.service';
 import { CommonHelper } from '../../Shared/Service/common-helper.service';
 import { HttpHelperService } from '../../Shared/Service/http-helper.service';
-import { TruncatePipe } from "../../Shared/Pipe/truncate.pipe";
-import { CartService } from '../../Shared/Service/cart.service';
-import { CartRequestDto } from '../../Model/Cart';
-import { CategoryWiseProductComponent } from "../category-wise-product/category-wise-product.component";
+import { TruncatePipe } from '../../Shared/Pipe/truncate.pipe';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-easy-to-buy-home',
+  selector: 'app-category-product',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterModule, TruncatePipe, CategoryWiseProductComponent],
-  templateUrl: './easy-to-buy-home.component.html',
-  styleUrl: './easy-to-buy-home.component.scss',
-  providers: [DatePipe, { provide: LOCALE_ID, useValue: 'en-US' }]
+  imports: [CommonModule, FormsModule, RouterModule, TruncatePipe],
+  templateUrl: './category-product.component.html',
+  styleUrl: './category-product.component.scss',
+  providers: [DatePipe, { provide: LOCALE_ID, useValue: 'en-US' }],
 })
-export class EasyToBuyHomeComponent implements OnInit {
-
+export class CategoryProductComponent implements OnInit {
   public oProductFilterDto = new ProductFilterDto();
   public oCartRequestDto = new CartRequestDto();
   public oCurrentUser = new UserResponseDto();
@@ -34,7 +32,8 @@ export class EasyToBuyHomeComponent implements OnInit {
     private http: HttpHelperService,
     private router: Router,
     private cartService: CartService,
-    private datePipe: DatePipe) {
+    private datePipe: DatePipe,
+  ) {
     this.oCurrentUser = CommonHelper.GetUser();
   }
 
@@ -43,26 +42,33 @@ export class EasyToBuyHomeComponent implements OnInit {
     this.GetProduct();
   }
 
-
   private GetProduct() {
     this.oProductFilterDto.companyId = Number(CommonHelper.GetComapyId());
-    this.oProductFilterDto.categoryId = Number(this.oProductFilterDto.categoryId);
-    this.oProductFilterDto.subCategoryId = Number(this.oProductFilterDto.subCategoryId);
-    this.oProductFilterDto.measurementUnitId = Number(this.oProductFilterDto.measurementUnitId);
-    this.oProductFilterDto.packTypeId = Number(this.oProductFilterDto.packTypeId);
-    this.oProductFilterDto.isActive = CommonHelper.booleanConvert(this.oProductFilterDto.isActive);
+    this.oProductFilterDto.categoryId = Number(
+      this.oProductFilterDto.categoryId,
+    );
+    this.oProductFilterDto.subCategoryId = Number(
+      this.oProductFilterDto.subCategoryId,
+    );
+    this.oProductFilterDto.measurementUnitId = Number(
+      this.oProductFilterDto.measurementUnitId,
+    );
+    this.oProductFilterDto.packTypeId = Number(
+      this.oProductFilterDto.packTypeId,
+    );
+    this.oProductFilterDto.isActive = CommonHelper.booleanConvert(
+      this.oProductFilterDto.isActive,
+    );
     // After the hash is generated, proceed with the API call
     this.http.Post(`Product/GetAllProducts`, this.oProductFilterDto).subscribe(
       (res: any) => {
         this.productList = res;
       },
       (err) => {
-        this.toast.error(err.ErrorMessage, "Error!!", { progressBar: true });
-      }
+        this.toast.error(err.ErrorMessage, 'Error!!', { progressBar: true });
+      },
     );
-
   }
-
 
   public GetImageUrl(fileId: number): string {
     return `${this.http.appUrl}UploadedFile/GetImage/${fileId}`;
@@ -76,14 +82,13 @@ export class EasyToBuyHomeComponent implements OnInit {
     this.oCartRequestDto.productId = product.id;
     this.oCartRequestDto.quantity = 1;
     this.oCartRequestDto.actionType = 1;
-    this.http.Post("Cart/InsertCart", this.oCartRequestDto).subscribe(
+    this.http.Post('Cart/InsertCart', this.oCartRequestDto).subscribe(
       (res) => {
         this.cartService.notifyCartUpdated(); // Notify cart update
       },
       (err) => {
-        this.toast.error(err.ErrorMessage, "Error!!", { progressBar: true });
-      }
+        this.toast.error(err.ErrorMessage, 'Error!!', { progressBar: true });
+      },
     );
   }
-
 }
---
+++ src/app/General/category-wise-product/category-wise-product.component.ts
@@ -0,0 +1,72 @@
+import { CommonModule, DatePipe } from '@angular/common';
+import { Component, OnInit, TrackByFunction } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from '../../Shared/Service/auth.service';
+import { CartService } from '../../Shared/Service/cart.service';
+import { HttpHelperService } from '../../Shared/Service/http-helper.service';
+import { FormsModule } from '@angular/forms';
+import { CategoryFilterRequestDto, CategoryRequestDto } from '../../Model/Category';
+import { AGGridHelper } from '../../Shared/Service/AGGridHelper';
+import { CommonHelper } from '../../Shared/Service/common-helper.service';
+import { CategoryComponent } from "../../Component/category/category.component";
+import { CategoryProductComponent } from "../category-product/category-product.component";
+
+@Component({
+  selector: 'app-category-wise-product',
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterModule, CategoryComponent, CategoryProductComponent],
+  templateUrl: './category-wise-product.component.html',
+  styleUrl: './category-wise-product.component.scss',
+  providers: [DatePipe],
+})
+export class CategoryWiseProductComponent implements OnInit {
+
+  public categoryList: any[] = [];
+  public oCategoryFilterRequestDto = new CategoryFilterRequestDto();
+  trackByFn: TrackByFunction<any> | any;
+  trackByCategory: TrackByFunction<any> | any;
+  trackByCategoryFrom: TrackByFunction<any> | any;
+  constructor(
+    public authService: AuthService,
+    private toast: ToastrService,
+    private http: HttpHelperService,
+    private router: Router,
+    private datePipe: DatePipe) {
+  }
+
+
+  ngOnInit(): void {
+    this.GetCategorys();
+  }
+
+
+
+  detailToGrid(params: any) {
+    const eDiv = document.createElement('div');
+    eDiv.innerHTML = ' <button class="btn btn-success p-0 px-1"> <i class="bi bi-eye-fill"></i> Detail</button>'
+    eDiv.addEventListener('click', () => {
+      this.router.navigateByUrl('transactions/' + params.data.transactionId)
+    });
+    return eDiv;
+  }
+
+
+  private GetCategorys() {
+    let currentUser = CommonHelper.GetUser();
+    this.oCategoryFilterRequestDto.companyId = Number(currentUser?.companyId);
+    this.oCategoryFilterRequestDto.parentId = 0;
+    this.oCategoryFilterRequestDto.isActive = CommonHelper.booleanConvert(this.oCategoryFilterRequestDto.isActive);
+    this.http.Post(`Category/GetAllCategories`, this.oCategoryFilterRequestDto).subscribe(
+      (res: any) => {
+        this.categoryList = res;
+      },
+      (err) => {
+        this.toast.error(err.ErrorMessage, "Error!!", { progressBar: true });
+      }
+    );
+
+  }
+
+
+}
