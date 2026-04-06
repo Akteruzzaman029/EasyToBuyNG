db3937f97ebaccfa6a1bb66574ee2a32e41f6094
omarcodezzi <omar.codezzi@gmail.com>
2026-04-07 03:35:00 +0600

    Address edit & Order get Add

--- src/app/General/my-order/my-order.component.ts
+++ src/app/General/my-order/my-order.component.ts
@@ -1,12 +1,58 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, signal } from '@angular/core';
+import { AuthService } from '../../Shared/Service/auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { HttpHelperService } from '../../Shared/Service/http-helper.service';
+import { Router } from '@angular/router';
+import { CommonModule, JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-my-order',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,JsonPipe],
   templateUrl: './my-order.component.html',
   styleUrl: './my-order.component.scss'
 })
 export class MyOrderComponent {
+  public orderList = signal<any[]>([]);  
+  public totalRecords: number = 0;
 
+  constructor(
+    public authService: AuthService,
+    private toast: ToastrService,
+    private http: HttpHelperService,
+    private router: Router,
+    private cdr: ChangeDetectorRef) {
+  }
+
+  ngOnInit(): void {
+    this.getOrders();
+  }
+
+  getOrders(page: number = 1) {
+    const userJson = localStorage.getItem("UserResponseDto");
+    const user = userJson ? JSON.parse(userJson) : null;
+
+    const searchModel = {
+      userId: user ? user.userId : null,
+      isActive: true
+    };
+
+    const url = `Order/GetOrder?pageNumber=${page}`;
+
+    this.http.Post(url, searchModel).subscribe({
+      next: (res: any) => {
+        debugger
+        if (res && res.items) {
+          this.orderList.set(res.items);
+          this.totalRecords = res.totalRecords;
+        } else {
+          this.orderList.set([]);
+        }
+        this.cdr.detectChanges();
+      },
+      error: (err) => {
+        console.error("Error loading orders:", err);
+      }
+    });
+  }
 }
